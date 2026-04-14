9d9ccc406880641e627baa9fa4a15191218c4b84
Nadya-Semyonova <gerey05@gmail.com>
2026-04-14 14:24:36 +0200

    Добавлен 404.html для маршрутизации

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
+import path from 'path'; 
+
+const copyIndexTo404 = () => ({
+  name: 'copy-index-to-404',
+  writeBundle() {
+    const indexHtml = path.resolve(__dirname, 'dist/index.html');
+    const notFoundHtml = path.resolve(__dirname, 'dist/404.html');
+    
+    if (fs.existsSync(indexHtml)) {
+      fs.copyFileSync(indexHtml, notFoundHtml);
+      console.log('✅ 404.html создан из index.html');
+    }
+  },
+});
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),copyIndexTo404()],
   base: '/products-app/',
   server: {
     port: 3000,
